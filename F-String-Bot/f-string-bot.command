f264c257dfb0168609b8755bf0076c9da909677e
Steven Wilson <stevenpeterwilson@gmail.com>
2020-08-20 21:56:43 -0600

    remove / from """ quoted strings, TODO list items

---
+++ F-String-Bot/f-string-bot.command
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+python3 /Users/stevenwilson/PycharmProjects/Reddit-Bots/comment_checker_aug18.py
--- F-String-Bot/submission_checker_aug18.py
+++ F-String-Bot/submission_checker_aug18.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python3
+
 import praw, re, os
 reddit = praw.Reddit("RSB1", user_agent="bot1 user agent")
 subreddit = reddit.subreddit('learnpython')
